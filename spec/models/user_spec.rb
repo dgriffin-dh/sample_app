0bf2ea806577a1faabc8da9dd31d286de3ed0526
Daniel Griffin <dg39@duke.edu>
2013-11-14 13:20:48 -0500

    Listing 6.28: user_spec updated for password length and authenticate

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,8 +14,11 @@ describe User do
 	it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
 	it { should be_valid }
+
+## Tests for Name
 
 	describe "when name is not present" do
 		before { @user.name = " " }
@@ -31,6 +34,8 @@ describe User do
 		before { @user.name = "a" * 51 }
 		it { should_not be_valid }
 	end
+
+## Tests for Email
 
 	describe "when email format is invalid" do
     it "should be invalid" do
@@ -63,6 +68,8 @@ describe User do
     it { should_not be_valid }
   end
 
+## Tests for Password
+
   describe "when password is not present" do
     before do
       @user = User.new(name: "Example User", email: "user@example.com",
@@ -75,4 +82,34 @@ describe User do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
+
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+
+  describe "with a password that's too long" do
+    before { @user.password = @user.password_confirmation = "a" * 129 }
+    it { should be_invalid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by(email: @user.email) }
+
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_false }
+    end
+  end
 end
+
+
+
+
