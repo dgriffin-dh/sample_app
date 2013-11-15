b1ff353a43f13b2563d898a8874a4f63de12055c
Daniel Griffin <dg39@duke.edu>
2013-11-15 12:41:34 -0500

    Listing 8.14: added SessionsHelper to App Controler

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -15,10 +15,15 @@ describe "Authentication" do
   	before { visit signin_path }
 
   	describe "with invalid information" do
-  		before { click_button }
+  		before { click_button "Sign In" }
 
   		it { should have_title('Sign In') }
-  		it { should have_selector('div.alert.alert-error', text: 'Invalid!') }
+  		it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
   	end
 
     describe "with valid information" do
