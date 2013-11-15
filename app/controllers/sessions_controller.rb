98803c026259b7186211ad12bc7d79eeea908e98
Daniel Griffin <dg39@duke.edu>
2013-11-15 12:26:38 -0500

    8.1.5: render flash error with invalid signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,13 @@ def new
 end
 
 def create
-	render 'new'
+	user = User.find_by(email: params[:sessions][:email].downcase)
+	if user && user.authenticate(params[:session][:password])
+		# Sign the user in and redirect to the user's show page.
+	else
+		flash[:error] = 'Invalid email/password combination'
+		render 'new'
+	end
 end
 
 def destroy
